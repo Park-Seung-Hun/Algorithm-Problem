ad7839c24520d5200ee8e4969c3dd6211d629016
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-05 23:01:02 +0900

    Bronze 5

---
+++ code.plus/1. SW 역량 테스트 준비 - 기초/수학/10430.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+	if (b == 1) return a;
+	else return gcd(b, a % b);
+}// 최대 공약수
+
+int lcm(int a, int b) {
+	int g = gcd(a, b);
+	return g * (a / g) * (b / g);
+}// 최소 공배수
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+
+	cout << gcd(A, B) << '\n';
+	cout << lcm(A, B) << '\n';
+}
+
+/*
+	백준 2690번 최대공약수와 최소공배수
+*/ 

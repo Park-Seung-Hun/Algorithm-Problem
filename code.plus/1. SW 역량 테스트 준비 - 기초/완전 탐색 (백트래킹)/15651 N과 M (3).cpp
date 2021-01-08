f6db94110077f058c846da2b3ded44f9e7e84e34
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-08 22:59:02 +0900

    Silver 3

---
+++ code.plus/1. SW 역량 테스트 준비 - 기초/완전 탐색 (백트래킹)/15651 N과 M (3).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> num;
+
+void back(int cnt) {
+	
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) cout << num[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+			num.push_back(i);
+			back(cnt + 1);
+			num.pop_back();
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	back(0);
+
+}
+/*
+	백준 15651번 N과 M (3)
+*/

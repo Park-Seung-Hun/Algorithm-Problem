710937c147df1dbe400947797435fdac900190ae
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-08 22:56:55 +0900

    Silver 3

---
+++ code.plus/1. SW 역량 테스트 준비 - 기초/완전 탐색 (백트래킹)/15649 N과 M (2).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> num;
+vector<bool> check;
+
+void back(int idx,int cnt) {
+	
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) cout << num[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = idx; i <= n; i++) {
+		if (check[i] == false) {
+			check[i] = true;
+			num.push_back(i);
+			back(i,cnt + 1);
+			num.pop_back();
+			check[i] = false;
+		}
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	check.resize(n + 1);
+	back(1,0);
+
+}
+/*
+	백준 15649번 N과 M (2)
+*/

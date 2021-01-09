9a7fa98a63546640abbb0a8bc25f98564e3f9bcf
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-09 12:15:41 +0900

    Silver 2

---
+++ code.plus/1. SW 역량 테스트 준비 - 기초/완전 탐색 (백트래킹)/15666 N과 M (12).cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> num;
+vector<int> save;
+map<string,int> M;
+
+bool str_check(string s) {
+	if (M[s] == 0) {
+		M[s] = 1;
+		return true;
+	}
+	else return false;
+}
+void back(int idx,int cnt) {
+	if (cnt == m) {
+		string t = "";
+		for (int i = 0; i < m; i++) t += (save[i]+'0');
+
+		if (str_check(t)) {
+			for (int i = 0; i < m; i++) cout << save[i] << ' ';
+		}
+		else return;
+
+		cout << '\n';
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		if (i >= idx) {
+			save.push_back(num[i]);
+			back(i,cnt + 1);
+			save.pop_back();
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	
+	num.resize(n);
+
+	for (int i = 0; i < n; i++) cin >> num[i];
+	sort(num.begin(), num.end());
+	
+	back(0,0);
+
+}
+/*
+	백준 15666번 N과 M (12)
+*/

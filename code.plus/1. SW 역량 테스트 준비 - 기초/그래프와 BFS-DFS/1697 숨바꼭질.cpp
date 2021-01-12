a577fd99e0e4e6a008e47e024db3849c12467811
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-12 09:02:54 +0900

    Silver 1

--- code.plus/1. SW 역량 테스트 준비 - 기초/그래프와 BFS-DFS/1697 숨바꼭질.cpp
+++ code.plus/1. SW 역량 테스트 준비 - 기초/그래프와 BFS-DFS/1697 숨바꼭질.cpp
@@ -1,44 +1,50 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <cmath>
 #include <algorithm>
 using namespace std;
-int x, y;
+
+int n, k;
 bool check[100001];
-void bfs(int x,int y) {
-	queue<pair<int,int> > q;
-	q.push(make_pair(x,0));
+
+void bfs(int x, int y) {
+	queue<pair<int, int> > q;
+	q.push(make_pair(x, 0));
 
 	while (!q.empty()) {
 		int line = q.front().first;
 		int time = q.front().second;
 		q.pop();
 
-		if (line == y) { 
+		if (line == y) {
 			cout << time;
-			return; 
+			return;
 		}
 
 		if (line + 1 <= 100000 && check[line + 1] == false) {
 			q.push(make_pair(line + 1, time + 1));
 			check[line + 1] = true;
 		}
-		if (line -1 >= 0 && check[line - 1] == false) {
+		if (line - 1 >= 0 && check[line - 1] == false) {
 			q.push(make_pair(line - 1, time + 1));
 			check[line - 1] = true;
 		}
-		if (2*line  <= 100000 && check[2*line] == false) {
-			q.push(make_pair(2*line, time + 1));
-			check[2*line] = true;
+		if (2 * line <= 100000 && check[2 * line] == false) {
+			q.push(make_pair(2 * line, time + 1));
+			check[2 * line] = true;
 		}
 
 	}
 }
 
-
 int main() {
-	cin >> x >> y;
-	bfs(x, y);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> k;
+	bfs(n, k);
 
-	return 0;
 }
+/*
+	백준 1697번 숨바꼭질
+*/

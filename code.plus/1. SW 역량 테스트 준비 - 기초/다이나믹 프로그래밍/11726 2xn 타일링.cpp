51a0835ef97ec3273d9feb2ea7bcd186e81184c8
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-23 09:59:13 +0900

    Silver 3

---
+++ code.plus/1. SW 역량 테스트 준비 - 기초/다이나믹 프로그래밍/11726 2xn 타일링.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001];
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	dp[0] = dp[1] = 1;
+	
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+
+	cout << dp[n]%10007;
+	
+}
+
+/*
+	백준 11726번 2xn 타일링
+*/

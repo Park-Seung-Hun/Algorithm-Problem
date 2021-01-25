775dd47eb001552c4c7b85d6223fb41133b30ff7
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-25 22:16:30 +0900

    Silver 2

---
+++ code.plus/1. SW 역량 테스트 준비 - 기초/다이나믹 프로그래밍/11722 가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int num[1000];
+int dp[1000];
+int n, ans;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> num[i];
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (num[i] < num[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans << '\n';
+}
+
+/*
+	백준 11722번 가장 긴 감소하는 부분 수열
+*/

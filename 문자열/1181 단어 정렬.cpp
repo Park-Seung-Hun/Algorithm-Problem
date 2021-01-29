08c5ac268ddcdc77bf43af3e7a6ac6644041bab3
Phodol2 <55614143+Park-Seung-Hun@users.noreply.github.com>
2021-01-29 21:07:15 +0900

    Silver 5

---
+++ 문자열/1181 단어 정렬.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<string> tt;
+int n;
+
+bool compare(string a, string b) {
+	if (a.size() == b.size()) return a < b; // 사전순 
+	return a.size() < b.size(); // 길이가 짧은순
+}
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string t;
+		cin >> t;
+		tt.push_back(t);
+	}
+
+	sort(tt.begin(), tt.end(), compare);
+
+	for (int i = 0; i < tt.size(); i++) {
+		if (i < tt.size() - 1 && tt[i].compare(tt[i + 1]) == 0) continue;
+		else cout << tt[i] << '\n';
+	}
+}
+/*
+    백준 1181번 단어 정렬
+*/

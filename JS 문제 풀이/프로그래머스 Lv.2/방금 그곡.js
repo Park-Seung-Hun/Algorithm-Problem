84837a709fd36f185ae9e2ceda0b4118e527fd93
ParkSH <psh19930326@gmail.com>
2021-04-22 19:57:55 +0900

    프로그래머스 문제풀이

---
+++ JS 문제 풀이/프로그래머스 Lv.2/방금 그곡.js
@@ -0,0 +1,56 @@
+function solution(m, musicinfos) {
+    let answer = [];
+    let checkTime=0;
+    /* 음악 제목, 재생이 시직되고 끝난 시각, 악보 */
+    /* C, C#, D, D#, E, F, F#, G, G#, A, A#, B */
+    const changeMusic = function(str){
+        let stack = [];
+        let arr = str.split('');
+
+        for(let i=0;i<arr.length;i++){
+            if(arr[i]==='#'){
+                stack.push(stack.pop().toLowerCase());
+            }
+            else stack.push(arr[i]);
+        }
+        return stack.join('');
+    }
+
+    m=changeMusic(m);
+    let arr = [] ;
+    let tmp = [] ;
+    for(let i=0;i<musicinfos.length;i++){
+        // 시작 , 종료 , 제목 , 악보 순으로 나누기
+        arr.push(musicinfos[i].split(','));
+
+        // 종료 - 시작 후 분으로 만들기
+        let startT = arr[i][0].split(':');
+        let start = parseInt(startT[0])*60+parseInt(startT[1]);
+
+        let endT = arr[i][1].split(':');
+        let end = parseInt(endT[0])*60+parseInt(endT[1]);
+        let Time = end-start;
+
+        arr[i][3]=changeMusic(arr[i][3]);
+        let music='';
+        // 음악 길이 >= 재생시간
+        if(arr[i][3].length>=Time){
+            music=arr[i][3].substr(0,Time);
+        }
+        // 음악 길이 < 재생시간
+        else{
+            let repeat = parseInt(Time/arr[i][3].length);
+            let module = parseInt(Time%arr[i][3].length);
+            for(let r=0;r<repeat;r++) music+=arr[i][3];
+            music+=arr[i][3].substr(0,module);
+        }
+
+        if(music.includes(m)&&checkTime<=Time) {
+            if(checkTime<Time)answer.unshift(arr[i][2]);
+            checkTime=Time;
+        }
+    }
+    if(answer.length===0) answer.push('(None)');
+
+    return answer[0];
+}

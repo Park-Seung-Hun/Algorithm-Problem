ba274cdf8fdf62bbb6582f89a005a4cdb327a7e7
ParkSH <psh19930326@gmail.com>
2021-06-04 10:40:35 +0900

    Solve DP Problem

---
+++ C++ 문제 풀이/다이나믹 프로그래밍/14916 거스름돈.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+string dp[1031];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	int cmp = n / 5;
+
+	int ans = -1;
+	for (int i = cmp; i >= 0; i--) {
+		int tmp = n - 5 * i;
+		if (tmp % 2 == 0) {
+			ans = i + tmp / 2;
+			break;
+		}
+	}
+	cout << ans;
+	return 0;
+}
